24c112af3d819e2e0c6f19fb3fb5100c168d1a18
Santosh Yadav <santosh.yadav198613@gmail.com>
2017-08-11 19:33:42 +0530

    added http event

--- src/app/services/todo/todo.service.ts
+++ src/app/services/todo/todo.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
 import { ToDo } from './todo';
 
 
@@ -18,6 +18,11 @@ export class ToDoService {
 
     addTodoItem(item: ToDo) {
         return this._http.post<ToDo>('https://jsonplaceholder.typicode.com/todos', item);
+    }
+
+    getData() {
+        const request = new HttpRequest('GET', 'https://jsonplaceholder.typicode.com/todos', { reportProgress: true });
+        return this._http.request(request);
     }
 
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ToDoService } from '../services/todo/todo.service';
 import { ToDo } from '../services/todo/todo';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-todo',
@@ -10,9 +11,18 @@ import { ToDo } from '../services/todo/todo';
 export class TodoComponent implements OnInit {
   todoList: any;
   todo = new ToDo();
+  percentLoaded: number;
   constructor(private _todoService: ToDoService) { }
 
   ngOnInit() {
+    // this._todoService.getData().subscribe(event => {
+    //   if (event.type === HttpEventType.DownloadProgress) {
+    //     this.percentLoaded = Math.round(100 * event.loaded / event.total);
+    //   }
+    //   else if (event instanceof HttpResponse) {
+    //     console.log(event);
+    //   }
+    // });
     this._todoService.getTodoList().subscribe(data => this.todoList = data);
   }
 
